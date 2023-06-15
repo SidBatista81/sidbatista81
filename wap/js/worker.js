0254b85f701441bcfc8cc4595ea1402395dd2add
SidBatista81 <108499308+SidBatista81@users.noreply.github.com>
2023-06-15 09:40:21 -0300

    Create worker.js na pasta WAP

---
+++ wap/js/worker.js
@@ -0,0 +1,29 @@
+var i = 0;
+
+onmessage=function(e){
+
+	postMessage("Aguarde...");	var f = function(){
+
+		if(f){
+
+			postMessage(++i);
+
+			setTimeout(f, 300); 
+
+		}else {
+
+			postMessage("Nāo definido");
+
+		}
+
+	};
+
+	f();
+
+	/*function (){
+
+		setTimeout(function (){postMessage(++i);}, 1000);
+
+	}();//*/
+
+};
