c2efdf574b7c98e76942783ef9ed5dcd357f269d
SidBatista81 <108499308+SidBatista81@users.noreply.github.com>
2023-06-16 09:41:57 -0300

    Update worker.js

--- wap/js/worker.js
+++ wap/js/worker.js
@@ -2,7 +2,7 @@
 var GHPATH = '/sidbatista81/wap';
  
 // Choose a different app prefix name
-var APP_PREFIX = 'gppwa_';
+var APP_PREFIX = 'wpasb81_';
  
 // The version of the cache. Every time you change any of the files
 // you need to change this version (version_01, version_02…). 
