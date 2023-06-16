35504ea92d3027d5c8620fe677b276c4d7c23835
SidBatista81 <108499308+SidBatista81@users.noreply.github.com>
2023-06-16 07:50:47 -0300

    Update worker.js

--- wap/js/worker.js
+++ wap/js/worker.js
@@ -1,31 +1,19 @@
-var i = 0;
-
-console.log("worker.js carregado")
-
-onmessage=function(e){
-
-	postMessage("Aguarde...");	var f = function(){
-
-		if(f){
-
-			postMessage(++i);
-
-			setTimeout(f, 300); 
-
-		}else {
-
-			postMessage("Nāo definido");
-
-		}
-
-	};
-
-	f();
-
-	/*function (){
-
-		setTimeout(function (){postMessage(++i);}, 1000);
-
-	}();//*/
-
-};
+// Change this to your repository name
+var GHPATH = '/sidbatista81/wap';
+ 
+// Choose a different app prefix name
+var APP_PREFIX = 'gppwa_';
+ 
+// The version of the cache. Every time you change any of the files
+// you need to change this version (version_01, version_02…). 
+// If you don't change the version, the service worker will give your
+// users the old files!
+var VERSION = 'version_00';
+ 
+// The files to make available for offline use. make sure to add 
+// others to this list
+var URLS = [    
+  `${GHPATH}/`,
+  `${GHPATH}/index.html`,
+  `${GHPATH}/js/main.js`
+]
