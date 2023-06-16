2c9d9354506bf6dd2e7f7c0ecc01e68f504b7a11
SidBatista81 <108499308+SidBatista81@users.noreply.github.com>
2023-06-15 23:44:56 -0300

    Update main.js

--- wap/js/main.js
+++ wap/js/main.js
@@ -1,14 +1,15 @@
 if(window.Worker){
-	navigator.serviceWorker.register('/js/worker.js');/*
-	
-	
-	var worker = ew Worker("js/worker.js");
+	navigator.serviceWorker.register('js/worker.js');/*
+	jar worker = ew Worker("js/worker.js");
 	worker.onmessage = function(e){
 		result.innerHTML = e.data;
 	};
+	
 	worker.postMessage("teste");
 	document.write(worker);
 	console.log("log"); //*/
+	
+	
 }
 
         
