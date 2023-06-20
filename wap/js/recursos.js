c3b55bcc0ddb0a29e6a565d37e1b103662c7999f
SidBatista81 <108499308+SidBatista81@users.noreply.github.com>
2023-06-20 08:36:26 -0300

    Incluso teste de acesso ao sistema de arquivos.

--- wap/js/recursos.js
+++ wap/js/recursos.js
@@ -1,2 +1,3 @@
-badges.innerHTML = navigator.setAppBadge ? '<a href="/sidbatista81/wap/recursos/selos.html">Selos</a>' : false;
+badges.innerHTML = navigator.setAppBadge ? '<a href="/sidbatista81/wap/recursos/selos.html">Selos</a>' : "Indisponível";
 notifications.innerHTML = ("Notification" in window) && '<a href="/sidbatista81/wap/recursos/notifica.html">Notificações</a>'
+arquivos.onclick = (async ()=>arquivos.innerHTML=(await window.showOpenFilePicker())[0].name)
